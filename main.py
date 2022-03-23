733c80f1b08bdaab918a24f1923701d6a6f13c34
Youcef Houssem Eddine Bousmaha <ky_bousmaha@esi.dz>
2022-03-23 12:08:56 +0300

    implementing the heuristic function h()

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ WIN = pygame.display.set_mode((SIZE, SIZE))
 
 pygame.display.set_caption('A* Path Finding Algorithm')
 
-# this is a spot in the grid
+
+# this is the heuristic function used in the A* pathfinding algorithm
+# We will use the manhattan distance
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1-x2) + abs(y1-y2)
